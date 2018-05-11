75bccc3322ca22b00a5a8efab59af9d90be12c7a
parshencev <parshencev.vlad@gmail.com>
2018-05-11 12:26:25 +0300

    ускорение алгоритма получения списка всех компаний клиента

--- src/server/mysql/functions/getActiveBankUserCompanies.sql
+++ src/server/mysql/functions/getActiveBankUserCompanies.sql
@@ -1,37 +1,24 @@
 BEGIN
-	DECLARE userID INT(11);
-	DECLARE dateEnd, dateStart VARCHAR(19);
-	DECLARE responce, distributionFilters, types JSON;
+	DECLARE userID INT(11) DEFAULT (SELECT user_id FROM connections WHERE connection_id = connectionID);
+	DECLARE responce, type, company JSON;
+	DECLARE distributionFilters JSON DEFAULT (SELECT state_json ->> "$.distribution" FROM states WHERE connection_id = connectionID);
+	DECLARE done TINYINT(1);
+	DECLARE companiesCursor CURSOR FOR SELECT company_json FROM companies WHERE (((type_id IN (15,16,17,24,25,26,27,28,29,30,31,32) AND company_date_create BETWEEN JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.api.dateStart")) AND JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.api.dateEnd"))) OR (type_id = 14 AND company_date_create BETWEEN JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.invalidate.dateStart")) AND JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.invalidate.dateEnd"))) OR (type_id = 37 AND company_date_create BETWEEN JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.difficult.dateStart")) AND JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.difficult.dateEnd"))) OR (type_id = 23 AND company_date_create BETWEEN JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.callBack.dateStart")) AND JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.callBack.dateEnd"))) OR (type_id IN (9, 35))) AND user_id = userID) OR (type_id = 36 AND company_date_create BETWEEN JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.notDial.dateStart")) AND JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.notDial.dateEnd"))) ORDER BY type_id;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 	SET responce = JSON_ARRAY();
-	SELECT state_json ->> "$.distribution" INTO distributionFilters FROM states WHERE connection_id = connectionID;
 	IF distributionFilters IS NOT NULL
 		THEN BEGIN
-			SELECT user_id INTO userID FROM connections WHERE connection_id = connectionID;
-			SET types = JSON_ARRAY(15,16,17,24,25,26,27,28,29,30,31,32); 
-			SET dateStart = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.api.dateStart"));
-			SET dateEnd = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.api.dateEnd"));
-			SET responce = getFilterCompaniesForUser(userID, types, dateStart, dateEnd);
-			SET types = JSON_ARRAY(14);
-			SET dateStart = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.invalidate.dateStart"));
-			SET dateEnd = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.invalidate.dateEnd"));
-			SET responce = JSON_MERGE(responce, getFilterCompaniesForUser(userID, types, dateStart, dateEnd));
-			SET types = JSON_ARRAY(36);
-			SET dateStart = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.notDial.dateStart"));
-			SET dateEnd = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.notDial.dateEnd"));
-			SET responce = JSON_MERGE(responce, getFilterCompaniesForUser(NULL, types, dateStart, dateEnd));
-			SET types = JSON_ARRAY(37);
-			SET dateStart = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.difficult.dateStart"));
-			SET dateEnd = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.difficult.dateEnd"));
-			SET responce = JSON_MERGE(responce, getFilterCompaniesForUser(userID, types, dateStart, dateEnd));
-			SET types = JSON_ARRAY(23);
-			SET dateStart = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.callBack.dateStart"));
-			SET dateEnd = JSON_UNQUOTE(JSON_EXTRACT(distributionFilters, "$.callBack.dateEnd"));
 			UPDATE companies SET type_id = 9, company_date_call_back = NULL WHERE user_id = userID AND type_id = 23 AND NOW() >= company_date_call_back;
-			SET responce = JSON_MERGE(responce, getFilterCompaniesForUser(userID, types, dateStart, dateEnd));
-			SET types = JSON_ARRAY(9);
-			SET responce = JSON_MERGE(responce, getFilterCompaniesForUser(userID, types, NULL, NULL));
-			SET types = JSON_ARRAY(35);
-			SET responce = JSON_MERGE(responce, getFilterCompaniesForUser(userID, types, NULL, NULL));
+			OPEN companiesCursor;
+				companiesLoop: LOOP
+					FETCH companiesCursor INTO company;
+					IF done 
+						THEN LEAVE companiesLoop;
+					END IF;
+					SET responce = JSON_MERGE(responce, company);
+					ITERATE companiesLoop;
+				END LOOP;
+			CLOSE companiesCursor;
 		END;
 	END IF;
 	RETURN responce;
