d8b9f6f9c545bdb4c7cd743904386bbca89aae63
parshencev <parshencev.vlad@gmail.com>
2018-05-15 17:01:24 +0300

    удаление лишней функции

--- src/server/mysql/astralInside.sql
+++ src/server/mysql/astralInside.sql
@@ -1387,26 +1387,6 @@ BEGIN
   RETURN responce;
 END$$
 
-CREATE DEFINER=`root`@`localhost` FUNCTION `getFilterCompaniesForUser` (`userID` INT(11), `types` JSON, `dateStart` VARCHAR(19) CHARSET utf8, `dateEnd` VARCHAR(19) CHARSET utf8) RETURNS JSON NO SQL
-BEGIN
-  DECLARE company, responce JSON;
-  DECLARE done TINYINT(1);
-  DECLARE companiesCursor CURSOR FOR SELECT company_json FROM companies WHERE  IF(dateStart IS NOT NULL AND dateEnd IS NOT NULL, DATE(company_date_create) BETWEEN dateStart AND dateEnd, 1) AND JSON_CONTAINS(types, CONCAT(type_id)) AND IF(userID IS NOT NULL, user_id = userID, 1) ORDER BY company_date_create DESC;
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-  SET responce = JSON_ARRAY();
-  OPEN companiesCursor;
-    companiesLoop: LOOP
-      FETCH companiesCursor INTO company;
-      IF done 
-        THEN LEAVE companiesLoop;
-      END IF;
-      SET responce = JSON_MERGE(responce, company);
-      ITERATE companiesLoop;
-    END LOOP;
-  CLOSE companiesCursor;
-  RETURN responce;
-END$$
-
 CREATE DEFINER=`root`@`localhost` FUNCTION `getHash` (`max` INT(3)) RETURNS VARCHAR(999) CHARSET utf8 NO SQL
 BEGIN
   DECLARE symbol VARCHAR(1);
