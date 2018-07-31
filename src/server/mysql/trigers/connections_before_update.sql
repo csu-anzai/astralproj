29e10e9d88bd48e0694e2de5ed40382ebd8d50b2
parshencev <parshencev.vlad@gmail.com>
2018-07-31 17:31:09 +0300

    фикс бага при выходе и заходе заново

--- src/server/mysql/trigers/connections_before_update.sql
+++ src/server/mysql/trigers/connections_before_update.sql
@@ -4,6 +4,9 @@ BEGIN
   	THEN SET NEW.connection_date_disconnect = NOW();
   END IF;
   IF NEW.user_id IS NOT NULL AND (NEW.user_id != OLD.user_id OR OLD.user_id IS NULL)
-  	THEN INSERT INTO states (connection_id, user_id) VALUES (NEW.connection_id, NEW.user_id);
+  	THEN BEGIN 
+  		DELETE FROM states WHERE connection_id = NEW.connection_id;
+  		INSERT INTO states (connection_id, user_id) VALUES (NEW.connection_id, NEW.user_id);
+  	END;
   END IF;
 END
