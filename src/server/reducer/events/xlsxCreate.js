00f569fac08f494eb97de597e90f6688445ad6a9
parshencev <parshencev.vlad@gmail.com>
2018-08-14 16:59:11 +0300

    создание файла из компаний в статистике

--- src/server/reducer/events/xlsxCreate.js
+++ src/server/reducer/events/xlsxCreate.js
@@ -2,25 +2,26 @@ const JE = require('json-xlsx');
 module.exports = modules => (resolve, reject, data) => {
 	const je = new JE({tmpDir: __dirname + '/../../express' + modules.env.express.staticPath});
 	je.write({name: data.name, data: data.data}, (err, filepath) => {
-    modules.log.writeLog("files", {
-        type: "createFile",
-        filepath
-    });
-    if (err) {
-    	reject(err);
-    } else {
-    	let fileName = filepath.match(".*/(.*\.xlsx)")[1];
-			filepath = `${modules.env.ws.location}:${modules.env.ws.port}/${fileName}`;
-    	modules.reducer.dispatch({
-    		type: "query",
-    		data: {
-    			query: "updateFileName",
-    			values: [
-    				data.fileID,
-    				filepath
-    			]
-    		}
-    	}).then(resolve).catch(reject);
-    }
+        modules.log.writeLog("files", {
+            type: "createFile",
+            filepath
+        });
+        if (err) {
+        	reject(err);
+        } else {
+        	let fileName = filepath.match(".*/(.*\.xlsx)")[1];
+    			filepath = `${modules.env.ws.location}:${modules.env.ws.port}/${fileName}`;
+            
+        	modules.reducer.dispatch({
+        		type: "query",
+        		data: {
+        			query: "updateFileName",
+        			values: [
+        				data.fileID,
+        				filepath
+        			]
+        		}
+        	}).then(resolve).catch(reject);
+        }
 	});
 }
