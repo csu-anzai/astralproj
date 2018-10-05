ee40e4ae6cc649f8be4f5a3f4c2a11ec71c5c559
parshencev <parshencev.vlad@gmail.com>
2018-10-05 14:07:13 +0300

    удаление компаний с номером телефона меньше 5-ти знаков

--- src/server/mysql/procedures/newCompanies.sql
+++ src/server/mysql/procedures/newCompanies.sql
@@ -88,7 +88,8 @@ BEGIN
 							PREPARE mysqlPrepare FROM @mysqlText;
 							EXECUTE mysqlPrepare;
 							DEALLOCATE PREPARE mysqlPrepare;
-							DELETE LOW_PRIORITY c from companies c, empty_companies_view ecv where c.company_id = ecv.company_id;
+							DELETE LOW_PRIORITY c FROM companies c, empty_companies_view ecv WHERE c.company_id = ecv.company_id;
+							DELETE LOW_PRIORITY FROM companies WHERE CHAR_LENGTH(company_phone) < 5;
 							SELECT count(*) INTO insertCompaniesCount FROM companies WHERE company_id > insertCompaniesCount;
 							UPDATE LOW_PRIORITY companies SET company_json = JSON_SET(company_json, "$.company_id", company_id) WHERE company_json ->> "$.company_id" = 0;
 							SET message = CONCAT(
