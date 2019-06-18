43de7732817e8572ccd57eead680edf56ae6328c
Denis Borodin <borodindk@gmail.com>
2019-06-18 19:43:40 +0300

    Форматирование телефонных номеров перед отправков в банки

--- src/presenters/company.js
+++ src/presenters/company.js
@@ -1,11 +1,18 @@
 // presenters Функция преобразования/форматирования объекта компании
+const formatPhoneNumber = require('../libs/formatPhoneNumber.js');
+const formatName = name => (
+  name[0].toUpperCase() + name.slice(1).toLowerCase()
+);
+
 module.exports = c => ({
   ...c,
   fio: [ /* добавляем ФИО */
     c.companyPersonSurname,
     c.companyPersonName,
     c.companyPersonPatronymic
-  ].map(name => (
-    name[0].toUpperCase() + name.slice(1).toLowerCase()
-  )).join(" ")
+  ].map(formatName).join(" "),
+  companyPersonSurname: formatName(c.companyPersonSurname),
+  companyPersonName: formatName(c.companyPersonName),
+  companyPersonPatronymic: formatName(c.companyPersonPatronymic),
+  phone: formatPhoneNumber(c.companyPhone)
 })
