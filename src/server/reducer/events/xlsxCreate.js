0462794c486eef828e312de3eaee9adc66efa7e7
Denis Borodin <borodindk@gmail.com>
2019-08-28 16:46:26 +0300

    Вывод только нужных колонок в xlsx

--- src/server/reducer/events/xlsxCreate.js
+++ src/server/reducer/events/xlsxCreate.js
@@ -1,5 +1,6 @@
 const JE = require('json-xlsx');
 module.exports = modules => (resolve, reject, data) => {
+    data.data = data.data.map(r => r.filter((c, key) => [0,1,4,10,24,26,35,44,46].includes(key))) // Оставляем только нужные колонки
 	const je = new JE({tmpDir: __dirname + '/../../express' + modules.env.express.staticPath});
 	je.write({name: data.name, data: data.data}, (err, filepath) => {
         modules.log.writeLog("files", {
@@ -11,7 +12,7 @@ module.exports = modules => (resolve, reject, data) => {
         } else {
         	let fileName = filepath.match(".*/(.*\.xlsx)")[1];
     			filepath = `${modules.env.ws.location}:${modules.env.ws.port}/${fileName}`;
-            
+
         	modules.reducer.dispatch({
         		type: "query",
         		data: {
